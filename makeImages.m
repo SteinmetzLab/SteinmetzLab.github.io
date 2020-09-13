11a4f0fb8439eb6692f490c770281bf33494ed3b
Nick Steinmetz <nick.steinmetz@gmail.com>
2020-09-12 18:21:12 -0700

    working makeImages, add pew and kling news

--- makeImages.m
+++ makeImages.m
@@ -3,19 +3,35 @@
 function makeImages(sourceImg)
 
 % load image data
-
+%%
 img = imread(sourceImg); 
 
-% is it too wide or 
+% is it too wide or too tall? 
 desRatio = 1920/1080; 
+thisRatio = size(img,2)/size(img,1); 
 
+% will fill with background color determined as the mode of all edge pixels
+bckgcolor = mode([squeeze(img(1,:,:)); ...
+    squeeze(img(end,:,:));...
+    squeeze(permute(img(:,1,:),[2 1 3])); ...
+    squeeze(permute(img(:,end,:),[2 1 3]))],1);
+bckgcolor = permute(bckgcolor, [1 3 2]);
+if thisRatio>desRatio % too wide, need to add rows
+    desRows = ceil(size(img,2)/desRatio); 
+    tempimg = repmat(bckgcolor, desRows, size(img,2), 1);
+    idx = floor((desRows-size(img,1))/2);
+    tempimg(idx:idx+size(img,1)-1,:,:) = img; 
+    img = tempimg;
+end
+
+%%
 [d,imName] = fileparts(sourceImg);
 [~,dest] = fileparts(d);
 
 outputDir = fullfile('assets', 'img', dest); 
 
 targets = struct(); 
-targets(end+1).width = 230; targets(end).suffix = 'placehold';
+targets(end).width = 230; targets(end).suffix = 'placehold';
 targets(end+1).width = 535; targets(end).suffix = 'thumb';
 targets(end+1).width = 535*2; targets(end).suffix = 'thumb@2x';
 targets(end+1).width = 575; targets(end).suffix = 'xs';
@@ -25,13 +41,15 @@ targets(end+1).width = 1999; targets(end).suffix = 'lg';
 targets(end+1).width = 1920; targets(end).suffix = '';
 
 qual = 70; 
+%%
 
+origW = size(img,2); 
 for ii = 1:numel(targets)
     if ~isempty(targets(ii).suffix)
-        fn = [imName '_' targets(ii).suffix]; 
+        fn = [imName '_' targets(ii).suffix '.jpg']; 
     else 
-        fn = imName; 
+        fn = [imName '.jpg']; 
     end
-    imgR = imresize(img, origW/targets(ii).width); 
+    imgR = imresize(img, targets(ii).width/origW); 
     imwrite(imgR, fullfile(outputDir,fn), 'JPEG', 'Quality', qual);
 end
