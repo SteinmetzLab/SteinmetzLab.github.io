0db7710c299e9952c30b697a08b245f37f2bc03f
Nick Steinmetz <nick.steinmetz@gmail.com>
2021-12-13 10:34:35 -0800

    add AnnaB image and first version of image script in matlab'

---
+++ img_script.m
@@ -0,0 +1,27 @@
+
+rootDir = '/Users/nicksteinmetz/Dropbox/code/SteinmetzLab.github.io/';
+sourceDir = fullfile(rootDir, '_img', 'people');
+
+targetDir = fullfile(rootDir, 'assets', 'img', 'people');
+
+name = 'AnnaB';
+
+d = dir(fullfile(sourceDir, [name '.*']));
+q = imread(fullfile(sourceDir, d(1).name));
+
+figure; image(q); 
+
+widthVals = [230 535 535*2 575 767 991 1999 1920];
+names = {'_placehold', '_thumb', '_thumb@2x', '_xs', '_sm', '_md', '_lg', ''}; 
+
+for ii = 1:numel(widthVals)
+
+x = widthVals(ii); 
+sc = x/size(q,2);
+b = imresize(q, sc);
+
+outFile = fullfile(targetDir, [name names{ii} '.jpg']);
+fprintf(1, 'Writing width %d to %s\n', x, outFile);
+imwrite(b, outFile, 'jpg', 'Quality', 70);
+
+end
