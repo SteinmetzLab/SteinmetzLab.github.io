4ac2a0ab15b47ba4d7a4ef20797b6f0cd9f5e9c9
Nick Steinmetz <nick.steinmetz@gmail.com>
2023-01-12 19:52:30 -0800

    add Kenneth and Jasmine photos

--- img_script.m
+++ img_script.m
@@ -1,17 +1,19 @@
 
 % source image should be aspect ratio 1920 wide x 1080 tall
 
-rootDir = '/Users/nicksteinmetz/Dropbox/code/SteinmetzLab.github.io/';
+% rootDir = '/Users/nicksteinmetz/Dropbox/code/SteinmetzLab.github.io/';
+rootDir = fullfile(dropboxDir, 'code','SteinmetzLab.github.io');
+
 sourceDir = fullfile(rootDir, '_img', 'people');
 
 targetDir = fullfile(rootDir, 'assets', 'img', 'people');
 
-name = 'Nyah';
+name = 'Jasmine';
 
 d = dir(fullfile(sourceDir, [name '.*']));
 q = imread(fullfile(sourceDir, d(1).name));
 
-figure; image(q); 
+figure; image(q); axis image; 
 
 widthVals = [230 535 535*2 575 767 991 1999 1920];
 names = {'_placehold', '_thumb', '_thumb@2x', '_xs', '_sm', '_md', '_lg', ''}; 
