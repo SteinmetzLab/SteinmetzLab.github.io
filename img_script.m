a765d7971e31e0359c8aa659f04ee80b5b27e5e9
Nick Steinmetz <nick.steinmetz@gmail.com>
2023-12-26 22:25:14 -0800

    update noPhoto to current logo

--- img_script.m
+++ img_script.m
@@ -1,7 +1,7 @@
 
 % source image should be aspect ratio 1920 wide x 1080 tall
 
-name = 'Bethania';
+name = 'noPhoto';
 type = 'people';
 
 % rootDir = '/Users/nicksteinmetz/Dropbox/code/SteinmetzLab.github.io/';
